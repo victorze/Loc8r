06b4da9aa02064747a07d04753773ee431befa9a
victorze <victorzevallos@protonmail.com>
2019-12-31 22:14:00 -0500

    Add api

---
+++ app_api/models/db.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+const host = process.env.DB_HOST || '127.0.0.1'
+const dbURL = `mongodb://${host}/Loc8r`;
+
+const connect = () => {
+  setTimeout(() => mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true }), 1000);
+}
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${dbURL}`);
+});
+
+mongoose.connection.on('error', err => {
+  console.log('Mongoose connection error:' + err);
+  return connect();
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+const gracefulShutdown = (msg, callback) => {
+  mongoose.connection.close( () => {
+    console.log(`Mongoose disconnected through ${msg}`);
+    callback();
+  });
+};
+
+process.once('SIGUSR2', () => {
+  gracefulShutdown('nodemon restart', () => {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+process.on('SIGINT', () => {
+  gracefulShutdown('app termination', () => {
+    process.exit(0);
+  });
+});
+process.on('SIGTERM', () => {
+  gracefulShutdown('Heroku app shutdown', () => {
+    process.exit(0);
+  });
+});
+
+connect();
+
+require('./locations');
