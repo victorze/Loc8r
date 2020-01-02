ae863dd184677d5d24c030f6ce62c287b9133038
victorze <victorzevallos@protonmail.com>
2020-01-02 18:17:53 -0500

    Add component that shows locations

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { HomeListComponent } from './home-list/home-list.component';
+import { DistancePipe } from './distance.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeListComponent
+    HomeListComponent,
+    DistancePipe
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [HomeListComponent]
 })
 export class AppModule { }
--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+
+export class Location {
+  _id: string;
+  name: string;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[];
+}
 
 @Component({
-  selector: 'app-home-list',
-  templateUrl: './home-list.component.html',
-  styleUrls: ['./home-list.component.css']
+  selector: "app-home-list",
+  templateUrl: "./home-list.component.html",
+  styleUrls: ["./home-list.component.css"]
 })
 export class HomeListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  locations: Location[] = [
+    {
+      _id: "590d8dc7a7cb5b8e3f1bfc48",
+      name: "Costy",
+      distance: 14000.1234,
+      address: "High Street, Reading",
+      rating: 3,
+      facilities: ["hot drinks", "food", "power"]
+    },
+    {
+      _id: "590d8dc7a7cb5b8e3f1bfc48",
+      name: "Starcups",
+      distance: 120.542,
+      address: "High Street, Reading",
+      rating: 5,
+      facilities: ["wifi", "food", "hot drinks"]
+    }
+  ];
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
