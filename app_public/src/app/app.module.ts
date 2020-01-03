0513c22e9a8f91f3bdf07b80e41bfaed0738d4bf
victorze <victorzevallos@protonmail.com>
2020-01-03 14:33:16 -0500

    Add Homepage component

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,20 +1,41 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
-import { HomeListComponent } from './home-list/home-list.component';
-import { DistancePipe } from './distance.pipe';
+import { HomeListComponent } from "./home-list/home-list.component";
+import { DistancePipe } from "./distance.pipe";
+import { FrameworkComponent } from "./framework/framework.component";
+import { AboutComponent } from "./about/about.component";
+import { HomepageComponent } from './homepage/homepage.component';
+import { PageHeaderComponent } from './page-header/page-header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     HomeListComponent,
-    DistancePipe
+    DistancePipe,
+    FrameworkComponent,
+    AboutComponent,
+    HomepageComponent,
+    PageHeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: "",
+        component: HomepageComponent
+      },
+      {
+        path: "about",
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [FrameworkComponent]
 })
-export class AppModule { }
+export class AppModule {}
