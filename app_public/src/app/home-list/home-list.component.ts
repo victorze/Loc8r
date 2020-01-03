9e6b3964adc4aaf06d121ce8a51ae8e8b5983d42
victorze <victorzevallos@protonmail.com>
2020-01-03 09:24:50 -0500

    Getting data from API and Put an Angular application into production

--- app_public/src/app/home-list/home-list.component.ts
+++ app_public/src/app/home-list/home-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Loc8rDataService } from "../loc8r-data.service";
 
 export class Location {
   _id: string;
@@ -15,26 +16,17 @@ export class Location {
   styleUrls: ["./home-list.component.css"]
 })
 export class HomeListComponent implements OnInit {
-  constructor() {}
+  constructor(private loc8rDataService: Loc8rDataService) {}
 
-  locations: Location[] = [
-    {
-      _id: "590d8dc7a7cb5b8e3f1bfc48",
-      name: "Costy",
-      distance: 14000.1234,
-      address: "High Street, Reading",
-      rating: 3,
-      facilities: ["hot drinks", "food", "power"]
-    },
-    {
-      _id: "590d8dc7a7cb5b8e3f1bfc48",
-      name: "Starcups",
-      distance: 120.542,
-      address: "High Street, Reading",
-      rating: 5,
-      facilities: ["wifi", "food", "hot drinks"]
-    }
-  ];
+  locations: Location[];
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getLocations();
+  }
+
+  private getLocations(): void {
+    this.loc8rDataService
+      .getLocations()
+      .then(foundLocations => (this.locations = foundLocations));
+  }
 }
